2104ffd823a057c7980b3ccd9e689685f97a5261
INSERR RPI <sinweitengisaac@gmail.com>
2023-06-08 06:40:40 -0400

    test script client side picamera2. TO DO: change to UDP 	new file:   TestPrograms/mjpeg_server.py 	modified:   TestPrograms/picamera2_rpi.py

--- TestPrograms/picamera2_rpi.py
+++ TestPrograms/picamera2_rpi.py
@@ -2,7 +2,6 @@
 
 import socket
 import time
-
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
@@ -12,9 +11,15 @@ video_config = picam2.create_video_configuration({"size": (1280, 720)})
 picam2.configure(video_config)
 encoder = H264Encoder(1000000)
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect(("169.254.196.165", 10001))
+with socket.socket() as sock:
+    sock.connect(("169.254.196.165", 8000))
+    print("connected")
     stream = sock.makefile("wb")
+    
+    picam2.start_preview()
+    time.sleep(2)
+    
+    
     picam2.start_recording(encoder, FileOutput(stream))
     time.sleep(20)
     picam2.stop_recording()
