44fa9dd596b870a9d94b521629b407a8ff71e591
IsaacSinn <sinweitengisaac@gmail.com>
2023-08-04 15:45:28 +0800

    Edited PiCameraServer to fix Streaming Handler Object not calllable, require testing

--- PiCameraServer.py
+++ PiCameraServer.py
@@ -17,7 +17,11 @@ from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
 
-class StreamingOutput(io.BufferedIOBase):
+class PiCameraServer(Module):
+    
+    def __init__(self):
+
+        class StreamingOutput(io.BufferedIOBase):
             def __init__(self):
                 self.frame = None
                 self.condition = Condition()
@@ -28,70 +32,67 @@ class StreamingOutput(io.BufferedIOBase):
                     self.condition.notify_all()
 
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
+        class StreamingHandler(server.BaseHTTPRequestHandler):
 
-    def __init__(self, output):
-        self.output = output
+            def __init__(self, output):
+                self.output = output
 
-        self.PAGE = """\
-        <html>
-        <head>
-        <title>picamera2 MJPEG</title>
-        </head>
-        <body>
-        <h1>Picamera2 MJPEG Streaming Demo</h1>
-        <img src="stream.mjpg" width="1280" height="720" />
-        </body>
-        </html>
-        """
-    
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = self.PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with self.output.condition:
-                        self.output.condition.wait()
-                        frame = self.output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
+                self.PAGE = """\
+                <html>
+                <head>
+                <title>picamera2 MJPEG</title>
+                </head>
+                <body>
+                <h1>Picamera2 MJPEG Streaming Demo</h1>
+                <img src="stream.mjpg" width="1280" height="720" />
+                </body>
+                </html>
+                """
+            
+            def do_GET(self):
+                if self.path == '/':
+                    self.send_response(301)
+                    self.send_header('Location', '/index.html')
                     self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+                elif self.path == '/index.html':
+                    content = self.PAGE.encode('utf-8')
+                    self.send_response(200)
+                    self.send_header('Content-Type', 'text/html')
+                    self.send_header('Content-Length', len(content))
+                    self.end_headers()
+                    self.wfile.write(content)
+                elif self.path == '/stream.mjpg':
+                    self.send_response(200)
+                    self.send_header('Age', 0)
+                    self.send_header('Cache-Control', 'no-cache, private')
+                    self.send_header('Pragma', 'no-cache')
+                    self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+                    self.end_headers()
+                    try:
+                        while True:
+                            with self.output.condition:
+                                self.output.condition.wait()
+                                frame = self.output.frame
+                            self.wfile.write(b'--FRAME\r\n')
+                            self.send_header('Content-Type', 'image/jpeg')
+                            self.send_header('Content-Length', len(frame))
+                            self.end_headers()
+                            self.wfile.write(frame)
+                            self.wfile.write(b'\r\n')
+                    except Exception as e:
+                        logging.warning(
+                            'Removed streaming client %s: %s',
+                            self.client_address, str(e))
+                else:
+                    self.send_error(404)
+                    self.end_headers()
 
 
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-
-class PiCameraServer(Module):
-    
-    def __init__(self):
+        class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+            allow_reuse_address = True
+            daemon_threads = True
+        
+        
         super().__init__()
 
         picam2 = Picamera2()
